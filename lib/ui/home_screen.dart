483238f57098f595c69caead850150bb1a9b8194
yso009 <yso009@naver.com>
2022-11-28 20:15:31 +0900

    searchForm 생성

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -8,10 +8,30 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('이미지 검색 앱'),
+        centerTitle: true,
+        title: const Text(
+          '이미지 검색 앱',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
       ),
-      body: Container(
-
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.search),
+                  )),
+            ),
+          ),
+        ],
       ),
     );
   }
