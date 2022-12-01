ac0c53ee6e1067761cc3a50aafc5bd6d29e28095
unknown <yso009@naver.com>
2022-12-01 15:59:24 +0900

    이미지 검색 기본 버전 완성

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,9 +1,37 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_search/model/Photo.dart';
 import 'package:image_search/ui/widget/photo_widget.dart';
+import 'package:http/http.dart' as http;
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _controller = TextEditingController();
+
+  List<Photo> _photos = [];
+
+  Future<List<Photo>> fetch(String query) async {
+    final response = await http.get(Uri.parse(
+        "https://pixabay.com/api/?key=31661655-a1566eb9ae408478812dcb42b&q=$query&image_type=photo&pretty=true"));
+
+    Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+    Iterable hits = jsonResponse['hits'];
+    return hits.map((e) => Photo.fromJson(e)).toList();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,12 +50,18 @@ class HomeScreen extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: TextField(
+              controller: _controller,
               decoration: InputDecoration(
                   border: const OutlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(10.0)),
                   ),
                   suffixIcon: IconButton(
-                    onPressed: () {},
+                    onPressed: () async {
+                      final photos = await fetch(_controller.text);
+                      setState(() {
+                        _photos = photos;
+                      });
+                    },
                     icon: const Icon(Icons.search),
                   )),
             ),
@@ -35,14 +69,17 @@ class HomeScreen extends StatelessWidget {
           Expanded(
             child: GridView.builder(
               padding: const EdgeInsets.all(16.0),
-              itemCount: 10,
+              itemCount: _photos.length,
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
                 crossAxisSpacing: 16,
                 mainAxisSpacing: 16,
               ),
               itemBuilder: (context, index) {
-                return const PhotoWidget();
+                final photo = _photos[index];
+                return PhotoWidget(
+                  photo: photo,
+                );
               },
             ),
           )
