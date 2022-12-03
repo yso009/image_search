10b225225c347c4966e982b8208980c74cb4bc84
yso009 <yso009@naver.com>
2022-12-03 18:24:03 +0900

    Stream을 활용하여 데이터 다루기

--- lib/data/photo_provider.dart
+++ lib/data/photo_provider.dart
@@ -1,10 +1,17 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:image_search/data/api.dart';
+import 'package:image_search/model/Photo.dart';
 
-class PhotoProvider extends InheritedWidget {
+
+class PhotoProvider extends InheritedWidget { //Inherited 안에는 불변 객체만 있을 수 있다.
   final PixabayApi api;
 
-  const PhotoProvider({
+  final _photoStreamController = StreamController<List<Photo>>()..add([]);
+  Stream<List<Photo>> get photoStream => _photoStreamController.stream;
+
+  PhotoProvider({
     Key? key,
     required this.api,
     required Widget child,
@@ -13,8 +20,13 @@ class PhotoProvider extends InheritedWidget {
   static PhotoProvider of(BuildContext context) {
     final PhotoProvider? result =
         context.dependOnInheritedWidgetOfExactType<PhotoProvider>();
-    assert(result != null, 'No PixabayApi found in context');
+    assert(result != null, 'No PixabayApi found in context'); 
     return result!;
+  }
+
+  Future<void> fetch(String query) async{
+    final result = await api.fetch(query);
+    _photoStreamController.add(result);
   }
 
   @override
--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -16,10 +16,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   final _controller = TextEditingController();
-
-  List<Photo> _photos = [];
 
   @override
   void dispose() {
@@ -29,7 +26,8 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final photoProvider = PhotoProvider.of(context); // photo_provider에 PhotoProvider를 얻어옴
+    final photoProvider =
+        PhotoProvider.of(context); // photo_provider에 PhotoProvider를 얻어옴
 
     return Scaffold(
       appBar: AppBar(
@@ -53,31 +51,39 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                   suffixIcon: IconButton(
                     onPressed: () async {
-                      final photos = await photoProvider.api.fetch(_controller.text);
-                      setState(() {
-                        _photos = photos;
-                      });
+                      photoProvider.fetch(_controller.text);
                     },
                     icon: const Icon(Icons.search),
                   )),
             ),
           ),
-          Expanded(
-            child: GridView.builder(
-              padding: const EdgeInsets.all(16.0),
-              itemCount: _photos.length,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-              ),
-              itemBuilder: (context, index) {
-                final photo = _photos[index];
-                return PhotoWidget(
-                  photo: photo,
-                );
-              },
-            ),
+          StreamBuilder<List<Photo>>(
+            stream: photoProvider.photoStream,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return const CircularProgressIndicator();
+              }
+
+              final photos = snapshot.data!;
+
+              return Expanded(
+                child: GridView.builder(
+                  padding: const EdgeInsets.all(16.0),
+                  itemCount: photos.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
+                  ),
+                  itemBuilder: (context, index) {
+                    final photo = photos[index];
+                    return PhotoWidget(
+                      photo: photo,
+                    );
+                  },
+                ),
+              );
+            }
           )
         ],
       ),
