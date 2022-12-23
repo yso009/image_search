4bcb7481db8bdb949b2664284c7943ca8b077cb7
unknown <yso009@naver.com>
2022-12-23 16:16:23 +0900

    test code 작성

--- test/ui/home_view_model_test.dart
+++ test/ui/home_view_model_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:image_search/data/api.dart';
+import 'package:image_search/model/Photo.dart';
 import 'package:image_search/ui/home_view_model.dart';
 
 void main() {
@@ -7,9 +8,12 @@ void main() {
     final viewModel = HomeViewModel(PixabayApi()); // 객체 생성
 
     await viewModel.fetch('apple');
+    await viewModel.fetch('apple');
 
-    expect(viewModel.photoStream, emitsInOrder([
-      
+    expect(viewModel.photoStream,
+        emitsInOrder([
+        isA<List<Photo>>(),
+        isA<List<Photo>>(),
     ]));
   });
 }
