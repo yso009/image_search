b5d3067dde0b4beecbc2f66f4e55d864322bccf9
unknown <yso009@naver.com>
2022-12-27 15:06:07 +0900

    refactor : ui - homescreen.dart 생성 1. ui를 담아놓을 폴더 생성 2. home_screen.dart 파일 생성 3. 폴더와 파일은 소문자 규칙을 지킬 것

---
+++ lib/ui/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          '이미지 검색 앱',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: Container(),
+    );
+  }
+}
