8565e472ec461d70b82953bc4cbbb73579f6e77d
yso009 <yso009@naver.com>
2022-12-29 19:13:27 +0900

    fetch 함수 api로 분리 1. fetch 함수 api로 분리 2. 분리한 api를 Class화 3. url, key 변수로 분리
    ** PixabayApi가 없이는 실행 안됨 == 아직까지는 의존성을 갖고 있다.

---
+++ lib/data/api.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:image_search/model/photo.dart';
+import 'package:http/http.dart' as http;
+
+class PixabayApi {
+  Future<List<Photo>> fetch(String query) async {
+    String q = query;
+    final baseUrl = 'https://pixabay.com/api/';
+    final key = '31661655-a1566eb9ae408478812dcb42b';
+
+    var response = await http.get(Uri.parse('$baseUrl?key=$key&q=$q&image_type=photo&pretty=true'));
+
+    Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+    Iterable hits = jsonResponse['hits'];
+
+    return hits.map((e) => Photo.fromJson(e)).toList();
+  }
+}
--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:image_search/data/api.dart';
 import 'package:image_search/model/photo.dart';
 import 'package:image_search/ui/widget/photo_widget.dart';
 import 'package:http/http.dart' as http;
@@ -13,23 +14,11 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final api = PixabayApi();
+
   final _controller = TextEditingController();
 
   List<Photo> _photos = [];
-
-  Future<List<Photo>> fetch(String query) async {
-    String q = query;
-    var url =
-        'https://pixabay.com/api/?key=31661655-a1566eb9ae408478812dcb42b&q=$q&image_type=photo&pretty=true';
-
-    var response = await http.get(Uri.parse(url));
-
-    Map<String, dynamic> jsonResponse = jsonDecode(response.body);
-    Iterable hits = jsonResponse['hits'];
-
-    print("hits: $hits");
-    return hits.map((e) => Photo.fromJson(e)).toList();
-  }
 
   @override
   void dispose() {
@@ -39,7 +28,6 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    fetch("apple");
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -62,7 +50,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 suffixIcon: IconButton(
                   onPressed: () async {
-                    final photos = await fetch(_controller.text);
+                    final photos = await api.fetch(_controller.text);
                     setState(() {
                       _photos = photos;
                     });
